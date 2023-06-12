9bcf32fcda692c6e41654b5470036a37e616576f
Yevgeniy Utkin <yevgeniy.utkin@gmail.com>
2023-06-12 22:00:49 +0300

    commit 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+const contacts = [
+    { id: nanoid(), name: 'Rosie Simpson' },
+    { id: nanoid(), name: 'Hermione Kline' },
+    { id: nanoid(), name: 'Eden Clements' },
+];
+
+export class App extends React.Component {
+    state = {
+        contacts: contacts,
+        name: '',
+    };
+
+    handleChange = event => {
+        this.setState(prevState => {
+            return {
+                name: event.target.value,
+            };
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const contact = {
+            id: nanoid(),
+            name: this.state.name,
+        };
+
+        this.setState(prevState => {
+            return {
+                contacts: [...prevState.contacts, contact],
+                name: '',
+            };
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Phonebook</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name
+                        <input
+                            type="text"
+                            name="name"
+                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                            required
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <button type="submit">Add contact</button>
+                </form>
+
+                <p>Contacts</p>
+                {this.state.contacts.map(contact => {
+                    return <div key={contact.id}>{contact.name}</div>;
+                })}
+            </div>
+        );
+    }
+}
